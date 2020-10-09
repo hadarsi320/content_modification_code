Added some fixes to the functions which compute the average rank promotion, and created a function which compares the results of different competitions (based on the amount of bots)

data_analysis.py
@@ -3,20 +3,24 @@ from collections import defaultdict
 import numpy as np
 
 
-def in_group(competitor, group, dummy_bots):
-    dummy_bots = ['DUMMY1' 'DUMMY2'] if dummy_bots == 'both' else \
-        (['DUMMY' + dummy_bots] if dummy_bots in ['1', '2'] else [])
+def in_group(competitor, group, dummy_bot):
+    dummy_bots = ['DUMMY1', 'DUMMY2'] if dummy_bot == 'both' else \
+        (['DUMMY' + dummy_bot] if dummy_bot in ['1', '2'] else [])
     bots = ['BOT'] + dummy_bots
+    planted_documents = ['DUMMY_1', 'DUMMY_2']
     return (group == 'bots' and competitor in bots) or \
            (group == 'students' and competitor not in bots) or \
            (group == 'true_bots' and competitor == 'BOT') or \
-           (group == 'dummy_bots' and competitor in dummy_bots)
+           (group == 'dummy_bots' and competitor in dummy_bots) or \
+           (group == 'planted' and competitor in planted_documents) or \
+           (group == 'actual_students' and competitor not in planted_documents and competitor != 'BOT')
 
 
 def get_scaled_promotion(last_rank, current_rank):
     if last_rank == current_rank:
         return 0
-    return (last_rank - current_rank) / (last_rank if last_rank > current_rank else (5 - last_rank))
+    return (last_rank - current_rank) / ((last_rank - 1) if last_rank > current_rank else (5 - last_rank))
+    # return (last_rank - current_rank) / max(last_rank, 5 - last_rank)
 
 
 def compute_average_rank(ranked_lists, competitors_lists, group, paper_data=False):
@@ -31,6 +35,7 @@ def compute_average_rank(ranked_lists, competitors_lists, group, paper_data=Fals
     for competition_id in ranked_lists:
         dummy_bot = None if paper_data else competition_id.split('_')[3]
         competition = ranked_lists[competition_id]
+
         competitors_list = competitors_lists[competition_id]
 
         for competitor in competitors_list:
@@ -39,9 +44,17 @@ def compute_average_rank(ranked_lists, competitors_lists, group, paper_data=Fals
 
             epoch_ranks = []
             for epoch in sorted(competition):
-                rank = competition[epoch].index(competitor)+1
+                rank = competition[epoch].index(competitor) + 1
                 epoch_ranks.append(rank)
             ranks.append(epoch_ranks)
+
+        # competition_ranks = defaultdict(list)
+        # for epoch in sorted(competition):
+        #     for i, pid in enumerate(competition[epoch]):
+        #         if in_group(pid, group, dummy_bot):
+        #             competition_ranks[pid].append(i+1)
+        # for pid in competition_ranks:
+        #     ranks.append(competition_ranks[pid])
 
     average_rank = np.average(ranks, axis=0)
     return average_rank
@@ -54,19 +67,39 @@ def compute_average_promotion(ranked_lists, competitors_lists, group, scaled=Fal
     :param scaled: if True: return average scaled promotion, if False: return average promotion
     :return: an array with 3 cells where the i-th cell is the promotion from i-th round to the i+1-th round
     """
-    rank_promotion = []
+    # rank_promotion = []
+    # for competition_id in ranked_lists:
+    #     dummy_bot = None if paper_data else competition_id.split('_')[3]
+    #     competition = ranked_lists[competition_id]
+    #     competitors_list = competitors_lists[competition_id]
+    #     for competitor in competitors_list:
+    #         if not in_group(competitor, group, dummy_bot):
+    #             continue
+    #         lst = []
+    #         for last_epoch, epoch in zip(sorted(competition), sorted(competition)[1:]):
+    #             last_rank = competition[last_epoch].index(competitor)+1
+    #             rank = competition[epoch].index(competitor)+1
+    #             lst.append(get_scaled_promotion(last_rank, rank) if scaled else (last_rank - rank))
+    #         rank_promotion.append(lst)
+    # average_rank_promotion = np.average(rank_promotion, axis=0)
+    # return average_rank_promotion
+
+    rank_promotion = defaultdict(list)
     for competition_id in ranked_lists:
         dummy_bot = None if paper_data else competition_id.split('_')[3]
         competition = ranked_lists[competition_id]
         competitors_list = competitors_lists[competition_id]
+
         for competitor in competitors_list:
             if not in_group(competitor, group, dummy_bot):
                 continue
-            lst = []
+
             for last_epoch, epoch in zip(sorted(competition), sorted(competition)[1:]):
-                last_rank = competition[last_epoch].index(competitor)+1
-                rank = competition[epoch].index(competitor)+1
-                lst.append(get_scaled_promotion(last_rank, rank) if scaled else (last_rank - rank))
-            rank_promotion.append(lst)
-    average_rank_promotion = np.average(rank_promotion, axis=0)
+                last_rank = competition[last_epoch].index(competitor) + 1
+                rank = competition[epoch].index(competitor) + 1
+                # if last_rank == 1 and group == 'bots':
+                #     continue
+                rank_promotion[epoch].append(get_scaled_promotion(last_rank, rank) if scaled else last_rank - rank)
+
+    average_rank_promotion = [np.average(rank_promotion[epoch]) for epoch in sorted(rank_promotion)]
     return average_rank_promotion

plotting.py
@@ -47,16 +47,6 @@ def word_similarity_analysis(sim_dir, show=False, plots_dir=None):
     rounds = max(len(lst) for lst in sim_lists[0])
     matrices = [np.array([lst for lst in sim_lists[i] if len(lst) == rounds]) for i in range(2)]
 
-    # averaged_mat = np.average(matrices[0], axis=0)
-    # plot(averaged_mat, title=f'Lexical Similarity',
-    #      x_label='Round', y_label='Cosine Similarity', show=show,
-    #      save_file=plots_dir+'/Lexical Similarity' if plots_dir else None)
-    #
-    # averaged_mat = np.average(matrices[1], axis=0)
-    # plot(averaged_mat, title=f'Embedding Similarity',
-    #      x_label='Round', y_label='Cosine Similarity', show=show,
-    #      save_file=plots_dir+'/Embedding Similarity' if plots_dir else None)
-
     titles = ['Lexical Similarity', 'Embedding Similarity']
     plt.rcParams.update({'font.size': 14})
     fig, axs = plt.subplots(ncols=2, figsize=(15, 5))
@@ -70,12 +60,12 @@ def word_similarity_analysis(sim_dir, show=False, plots_dir=None):
         axs[i].set_ylabel('Cosine Similarity')
 
     if plot:
-        plt.savefig(plots_dir+'/Similarity Plot')
+        plt.savefig(plots_dir + '/Similarity Plot')
     if show:
         plt.show()
 
 
-def competition_2of5_analysis(trec_dir, show=True, plots_dir=None):
+def competition_5_analysis(trec_dir, show=True, plots_dir=None):
     trec_files = sorted(listdir(trec_dir))
     ranked_lists = {trec_file: read_competition_trec_file(trec_dir + trec_file) for trec_file in trec_files}
     normalize_dict_len(ranked_lists)
@@ -102,7 +92,7 @@ def competition_2of5_analysis(trec_dir, show=True, plots_dir=None):
             result = results[group]
             label = labels[group]
             color = colors[group]
-            fig_num = int(j/2)
+            fig_num = int(j / 2)
             axs[fig_num].plot(x_ticks, result, label=label, color=color)
 
         fig.suptitle(title)
@@ -119,18 +109,25 @@ def competition_2of5_analysis(trec_dir, show=True, plots_dir=None):
             plt.show()
 
 
-def analyze_paper_competition(positions_file, show=True, plots_dir=None):
+def recreate_paper_plots(positions_file, show=True, plots_dir=None):
+    """
+    This function recreates the plots from the paper, based on the positions file from the paper
+    :param positions_file:
+    :param show:
+    :param plots_dir:
+    :return:
+    """
     ranked_lists = read_positions_file(positions_file)
     normalize_dict_len(ranked_lists)
     competitors_lists = {qid: next(iter(ranked_lists[qid].values())) for qid in ranked_lists}
 
-    groups = ['bots', 'students']
-    colors = {'bots': 'b', 'students': 'g'}
-    labels = {'bots': 'All Bots', 'students': 'Student Documents'}
+    groups = ['actual_students', 'planted', 'bots']
+    colors = {'bots': 'b', 'actual_students': 'r', 'planted': 'g'}
+    labels = {'bots': 'All Bots', 'actual_students': 'Student Documents', 'planted': 'Planted Documents'}
     functions = [lambda x: compute_average_rank(ranked_lists, competitors_lists, x, paper_data=True),
                  lambda x: compute_average_promotion(ranked_lists, competitors_lists, x, scaled=False, paper_data=True),
                  lambda x: compute_average_promotion(ranked_lists, competitors_lists, x, scaled=True, paper_data=True)]
-    titles = ['Average Rank', 'Average Raw Rank Promotion', 'Average Scaled Rank Promotion']
+    titles = ['Average Rank', 'Raw Rank Promotion', 'Scaled Rank Promotion']
     x_ticks_list = [range(1, 5), range(2, 5), range(2, 5)]
 
     for i in range(3):
@@ -142,6 +139,11 @@ def analyze_paper_competition(positions_file, show=True, plots_dir=None):
         results = {}
         for group in groups:
             results[group] = function(group)
+
+        print(title)
+        for group in results:
+            print(f'{group}: {results[group]}')
+        print()
 
         plt.rcParams.update({'font.size': 14})
         for j, group in enumerate(groups):
@@ -161,17 +163,87 @@ def analyze_paper_competition(positions_file, show=True, plots_dir=None):
             plt.show()
 
 
+def compare_competitions(postitions_file_1of5, trec_dir_2of5, trec_dir_3of5, show=True, plots_dir=None):
+    ranked_lists_1of5 = read_positions_file(postitions_file_1of5)
+    normalize_dict_len(ranked_lists_1of5)
+    competitors_lists_1of5 = {qid: next(iter(ranked_lists_1of5[qid].values())) for qid in ranked_lists_1of5}
+
+    trec_files = sorted(listdir(trec_dir_2of5))
+    ranked_lists_2of5 = {trec_file: read_competition_trec_file(trec_dir_2of5 + trec_file) for trec_file in trec_files}
+    normalize_dict_len(ranked_lists_2of5)
+    competitors_lists_2of5 = {trec_file: get_competitors(trec_dir_2of5 + trec_file) for trec_file in trec_files}
+
+    trec_files = sorted(listdir(trec_dir_3of5))
+    ranked_lists_3of5 = {trec_file: read_competition_trec_file(trec_dir_3of5 + trec_file) for trec_file in trec_files}
+    normalize_dict_len(ranked_lists_3of5)
+    competitors_lists_3of5 = {trec_file: get_competitors(trec_dir_3of5 + trec_file) for trec_file in trec_files}
+
+    ranked_lists_list = [ranked_lists_1of5, ranked_lists_2of5, ranked_lists_3of5]
+    competitors_lists_list = [competitors_lists_1of5, competitors_lists_2of5, competitors_lists_3of5]
+
+    groups = ['students', 'bots']
+    colors_list = [{'bots': 'blue', 'students': '#00a6ff'},
+                   {'bots': 'red', 'students': 'orange'},
+                   {'bots': 'green', 'students': '#00ff13'}]
+    labels_list = [{'bots': 'Bots 1/5', 'students': 'Students 1/5'},
+                   {'bots': 'Bots 2/5', 'students': 'Students 2/5'},
+                   {'bots': 'Bots 3/5', 'students': 'Students 3/5'}]
+    functions = [lambda x, y, z, w: compute_average_rank(x, y, z, paper_data=w),
+                 lambda x, y, z, w: compute_average_promotion(x, y, z, scaled=False, paper_data=w),
+                 lambda x, y, z, w: compute_average_promotion(x, y, z, scaled=True, paper_data=w)]
+    titles = ['Average Rank', 'Raw Rank Promotion', 'Scaled Rank Promotion']
+    x_ticks_list = [range(1, 5), range(2, 5), range(2, 5)]
+
+    fig, axs = plt.subplots(ncols=3, figsize=(18, 5))
+    plt.rcParams.update({'font.size': 14})
+
+    # for i in range(3):
+    for i in range(2):
+        ranked_lists = ranked_lists_list[i]
+        competitors_lists = competitors_lists_list[i]
+        labels = labels_list[i]
+        colors = colors_list[i]
+
+        for ii in range(3):
+            axis = axs[ii]
+            x_ticks = x_ticks_list[ii]
+            function = functions[ii]
+            title = titles[ii]
+
+            results = {group: function(ranked_lists, competitors_lists, group, i == 0) for group in groups}
+
+            for group in groups:
+                result = results[group]
+                label = labels[group]
+                color = colors[group]
+                axis.plot(x_ticks, result, label=label, color=color)
+
+            axis.set_title(title)
+            axis.set_ylabel(title)
+            axis.legend()
+            axis.set_xticks(x_ticks)
+            axis.set_xlabel('Round')
+
+    if plots_dir:
+        plt.savefig(plots_dir + '/competitions_comparison')
+    if show:
+        plt.show()
+
+
 def main():
-    trec_dir = 'output/2of5/run_10_3/trec_files/'
     sim_dir = 'output/2of2/run_10_3/similarity_results/'
+    trec_dir_2 = 'output/2of5/run_10_3/trec_files/'
+    trec_dir_3 = 'output/3of5/run_10_5/trec_files/'
     positions_file = 'data/paper_data/documents.positions'
 
     plots_dir = './plots'
     ensure_dir(plots_dir)
 
-    # competition_2of5_analysis(trec_dir, show=True, plots_dir=None)
+    # competition_2of5_analysis(trec_dir_2, show=True, plots_dir=None)
+    # competition_5_analysis(trec_dir_3, show=True, plots_dir=None)
     # word_similarity_analysis(sim_dir, show=True, plots_dir=plots_dir)
-    analyze_paper_competition(positions_file, show=True)
+    # recreate_paper_plots(positions_file, show=True)
+    compare_competitions(positions_file, trec_dir_2, trec_dir_3, show=True)
 
 
 if __name__ == '__main__':